1a1452c29d9e0c458b9d6ce204c0e460452793d5
mecaaww <rimamarsyaumm@gmail.com>
2024-06-03 20:10:29 +0700

    first commit

---
+++ src/books/HistoryBook.java
@@ -0,0 +1,9 @@
+package books;
+
+public class HistoryBook extends Book {
+    String category = "History";
+    public HistoryBook(String bookId, String title, String author, int stock) {
+        super(bookId, title, author, stock);
+        setCategory(category);
+    }
+}
---
+++ src/data/Admin.java
@@ -0,0 +1,8 @@
+package data;
+
+public class Admin {
+    private static String adminUsername = "admin";
+    private static String adminPassword = "123";
+
+    public static Student[] studentList = new Student[500];
+
